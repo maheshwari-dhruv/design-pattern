3454c8f63be7f4af665a970d1ad36e5dd63e3a81
Dhruv Maheshwari <mrdm026@gmail.com>
2023-02-22 18:02:53 +0530

    Initial files added for factory method pattern

---
+++ design-pattern/factory-method/src/main/java/com/learn/pattern/Main.java
@@ -0,0 +1,11 @@
+package com.learn.pattern;
+
+/**
+ * @author Dhruv Maheshwari
+ * @version \$Id: Main.java, v 0.1 2023-02-22 Dhruv Maheshwari Exp $$
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
