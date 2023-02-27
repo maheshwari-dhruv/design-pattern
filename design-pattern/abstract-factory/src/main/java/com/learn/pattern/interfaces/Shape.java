d9d089160153f3721db1b1b5fc427a123328aa57
Dhruv Maheshwari <mrdm026@gmail.com>
2023-02-27 16:10:16 +0530

    Step 1 - Create an interface for Shapes

---
+++ design-pattern/abstract-factory/src/main/java/com/learn/pattern/interfaces/Shape.java
@@ -0,0 +1,9 @@
+package com.learn.pattern.interfaces;
+
+/**
+ * @author Kaustubh Dongare
+ * @version $Id: Shape.java, v 0.1 2023-02-27 03:57 PM Kaustubh Dongare Exp $$
+ */
+public interface Shape {
+    void draw();
+}
